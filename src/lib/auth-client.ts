04859e09dae81529129e9c6f7ff8b96dbf8f534f
rashedul islam <rashedulislam@rasheduls-MacBook-Pro.local>
2026-02-04 22:04:47 +0600

    Update API rewrites and correct authentication client base URL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     //baseURL: "http://localhost:5007/api/auth"
-    baseURL: "https://assignment4-lake-eta.vercel.app"
+    baseURL: "https://client-hazel-theta.vercel.app/api/auth"
 })
